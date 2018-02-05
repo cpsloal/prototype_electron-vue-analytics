8fda07b33386afe5f9bf414169c25d837e22c495
Ali Satter <cpsloal@users.noreply.github.com>
2018-02-04 21:58:04 -0800

    Initial Checkin - Scaffolding from https://github.com/SimulatedGREG/electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'prototype_vue_analytics'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
